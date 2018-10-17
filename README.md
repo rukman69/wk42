3ae9cd6d58f639583310d5c61dee5a63235b9a7c
rukman69 <43822774+rukman69@users.noreply.github.com>
2018-10-17 19:17:51 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# wk42
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,61 @@
+function log(content) {
+  console.log(content);
+}
+var mince = [1, 2, 3, 4, 5, 6];
+
+var car = "Fiat";
+var person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+var x = 5;
+var y = 5;
+var z = 6;
+
+log(car), log(person.lastName);
+
+const eshop = {
+  name: "ïphone_world",
+  majitel: {
+    meno: "zdenoCiger",
+    adresa: "novomeskeho_21",
+    telefon: 905987678
+  },
+  sklad: [
+    {
+      nazov: "iphone5",
+      cena: "340"
+    },
+
+    {
+      nazov: "iphone6",
+      cena: "440"
+    },
+    {
+      nazov: "iphone7",
+      cena: "540"
+    }
+  ]
+};
+
+const zakaznik = {
+  meno: "zajko bojko",
+  tel: 903887766,
+  email: "yajobojko@gmail.com",
+  historia_nakupov: [
+    {
+      polozka: "ïphone5",
+      cena: "340",
+      zlava: false,
+      datum_nakupu: "2018-11-11"
+    },
+    {
+      polozka: "ïphone5",
+      cena: "340",
+      zlava: false,
+      datum_nakupu: "2018-11-11"
+    }
+  ]
+};
